b2daf93f6e4cef1c61f1d7bf24491c73e8c1ac21
qhtkdrma <kimwonkyu15@gmail.com>
2023-05-09 17:53:13 +0900

    val_loss 오류 해결

--- scr/training/cnnAutoml.py
+++ scr/training/cnnAutoml.py
@@ -1,4 +1,5 @@
 from scr.training.pretreatment import pretreatment, sentiment_predict, model_loss
+from sklearn.model_selection import train_test_split
 import autokeras as ak
 import numpy as np
 import tensorflow as tf
@@ -6,7 +7,9 @@ import tensorflow as tf
 
 class CNNAutoml:
     def Multiautokeras(self, X_train, X_test, y_train, y_test):
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
         X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+        X_val = np.reshape(X_val, (X_val.shape[0], X_val.shape[1], 1))
 
         input_node = ak.Input()
         output_node = ak.ConvBlock()(input_node)
@@ -21,7 +24,13 @@ class CNNAutoml:
             directory='../../data/model/autokeras_cnn',
         )
 
-        history = auto_model.fit(X_train, y_train, epochs=20, validation_split=0.2)
+        history = auto_model.fit(
+            X_train,
+            y_train,
+            epochs=20,
+            validation_data=(X_val, y_val)
+        )
+
         loaded_model = auto_model.export_model()
         tf.keras.models.save_model(loaded_model, '../../data/model/automlkeras_cnn_best_model.h5', save_format='h5')
         print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
@@ -33,7 +42,7 @@ class CNNAutoml:
     def __init__(self):
         X_train, X_test, y_train, y_test, vocab_size, max_len, stopwords, tokenizer = pretreatment()
 
-        loaded_model_automd = self.Multiautokeras(vocab_size, X_train, X_test, y_train, y_test, max_len)
+        loaded_model_automd = self.Multiautokeras(X_train, X_test, y_train, y_test)
 
         sentiment_predict("미친 새끼 또 저 지랄이네 대단하다 대단해", loaded_model_automd, stopwords, tokenizer, max_len)
         sentiment_predict("안녕하세요 오랜만에 뵈요. 어떤일 하고 있나요?", loaded_model_automd, stopwords, tokenizer, max_len)
--- scr/training/rnnAutoml.py
+++ scr/training/rnnAutoml.py
@@ -1,5 +1,6 @@
 # 전처리 코드 불러오기
 from scr.training.pretreatment import pretreatment, sentiment_predict, model_loss
+from sklearn.model_selection import train_test_split
 import autokeras as ak
 import numpy as np
 import tensorflow as tf
@@ -8,7 +9,9 @@ import tensorflow as tf
 class RNNAutoml:
     # autokeras로 분류하기
     def AutokerasLearn(self, X_train, X_test, y_train, y_test):
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
         X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+        X_val = np.reshape(X_val, (X_val.shape[0], X_val.shape[1], 1))
 
         input_node = ak.Input()
         output_node = ak.RNNBlock(return_sequences=False)(input_node)
@@ -23,7 +26,13 @@ class RNNAutoml:
             directory='../../data/model/autokeras_rnn',
         )
 
-        history = auto_model.fit(X_train, y_train, epochs=20, validation_split=0.2)
+        history = auto_model.fit(
+            X_train,
+            y_train,
+            epochs=20,
+            validation_data=(X_val, y_val)
+        )
+
         loaded_model = auto_model.export_model()
         tf.keras.models.save_model(loaded_model, '../../data/model/automlkeras_rnn_best_model.h5', save_format='h5')
         print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
