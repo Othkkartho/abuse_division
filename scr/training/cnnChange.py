9dcfa0ad5ab1ab722fabfdfd915741b8eadaa0a3
qhtkdrma <kimwonkyu15@gmail.com>
2023-05-11 21:34:13 +0900

    change 코드에 return 안해서 생기는 오류 수정, 학습에 걸리는 시간 측정 확인

--- scr/training/cnnChange.py
+++ scr/training/cnnChange.py
@@ -5,13 +5,12 @@ from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import time
 
 
 class CNN:
-    def multiKernel(self, vocab_size, X_train, X_test, y_train, y_test, max_len):
-        embedding_dim = 32
+    def multiKernel(vocab_size, X_train, X_test, y_train, y_test, max_len, embedding_dim, num_filters, batch_size):
         dropout_ratio = (0.5, 0.8)
-        num_filters = 32
         hidden_units = 128
 
         model_input = Input(shape=(max_len,))
@@ -41,22 +40,21 @@ class CNN:
         mc = ModelCheckpoint('../../data/model/CNN_mk_model.h5', monitor='val_acc', mode='max', verbose=1,
                              save_best_only=True)
 
-        history = model.fit(X_train, y_train, batch_size=64, epochs=10, validation_split=0.2, verbose=2,
+        history = model.fit(X_train, y_train, batch_size=batch_size, epochs=5, validation_split=0.2, verbose=2,
                             callbacks=[es, mc])
 
         loaded_model = load_model('../../data/model/CNN_mk_model.h5')
-        print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
+        loss, final_acc = loaded_model.evaluate(X_test, y_test)
+        print("\n 테스트 정확도: %.4f" % final_acc)
 
-        model_loss(history)
+        # model_loss(history)
 
-        return loaded_model
+        return loaded_model, loss, final_acc
 
     # 1D CNN으로 욕설 예측해보기
 
-    def onedcnn(self, vocab_size, X_train, X_test, y_train, y_test, max_len):
-        embedding_dim = 32
+    def onedcnn(vocab_size, X_train, X_test, y_train, y_test, max_len, embedding_dim, num_filters, batch_size):
         dropout_ratio = 0.3
-        num_filters = 32
         kernel_size = 5
 
         model = Sequential()
@@ -72,7 +70,7 @@ class CNN:
         mc = ModelCheckpoint('../../data/model/CNN_od_model.h5', monitor='val_acc', mode='max', verbose=1,
                              save_best_only=True)
 
-        history = model.fit(X_train, y_train, batch_size=64, epochs=10, validation_split=0.2, verbose=2,
+        history = model.fit(X_train, y_train, batch_size=batch_size, epochs=5, validation_split=0.2, verbose=2,
                             callbacks=[es, mc])
 
         str_X_train = [str(x) for x in X_train]
@@ -82,26 +80,57 @@ class CNN:
         str_X_test = [str(x) for x in X_test]
         X_test_encoded = tokenizer.texts_to_sequences(str_X_test)
         X_test_padded = pad_sequences(X_test_encoded, maxlen=max_len)
-        print("\n 테스트 정확도: %.4f" % (model.evaluate(X_test_padded, y_test)[1]))
 
         loaded_model = load_model('../../data/model/CNN_od_model.h5')
+        loss, final_acc = loaded_model.evaluate(X_test, y_test)
 
-        model_loss(history)
+        print("\n 테스트 정확도: %.4f" % final_acc)
 
-        return loaded_model
+        # model_loss(history)
 
-    def __init__(self):
+        return loaded_model, loss, final_acc
+
+    def start(embedding_dim, num_filters, batch_size):
+        result = []
         X_train, X_test, y_train, y_test, vocab_size, max_len, stopwords, tokenizer = pretreatment()
+
+        result.append(embedding_dim)
+        result.append(num_filters)
+        result.append(batch_size)
 
         max_len = 60
 
-        loaded_model_mk = self.multiKernel(vocab_size, X_train, X_test, y_train, y_test, max_len)
-        loaded_model_od = self.onedcnn(vocab_size, X_train, X_test, y_train, y_test, max_len)
+        start = time.time()
+        loaded_model_mk, m_loss, final_acc_m = CNN.multiKernel(vocab_size, X_train, X_test, y_train, y_test, max_len, embedding_dim, num_filters, batch_size)
+        loaded_model_od, o_loss, final_acc_o = CNN.onedcnn(vocab_size, X_train, X_test, y_train, y_test, max_len, embedding_dim, num_filters, batch_size)
+        end = time.time() - start
+
+        result.append(m_loss)
+        result.append(final_acc_m)
+        result.append(o_loss)
+        result.append(final_acc_o)
+        result.append(end)
 
         # sentiment_predict("미친 새끼 또 저 지랄이네 대단하다 대단해", loaded_model_mk, stopwords, tokenizer, max_len)
         # sentiment_predict("미친 새끼 또 저 지랄이네 대단하다 대단해", loaded_model_od, stopwords, tokenizer, max_len)
         # sentiment_predict("안녕하세요 오랜만에 뵈요. 어떤일 하고 있나요?", loaded_model_mk, stopwords, tokenizer, max_len)
         # sentiment_predict("안녕하세요 오랜만에 뵈요. 어떤일 하고 있나요?", loaded_model_od, stopwords, tokenizer, max_len)
 
+        return result
 
-CNN()
+
+cnn = []
+for ci in range(0, 9, 2):
+    for cj in range(0, 9, 2):
+        for ck in range(0, 9, 2):
+            embedding_dim = 2 ** ci
+            num_filters = 2 ** cj
+            batch_size = 2 ** ck
+
+            print(embedding_dim, num_filters, batch_size)
+
+            cnn.append(CNN.start(embedding_dim, num_filters, batch_size))
+
+for lists in cnn:
+    print("embedding_dim: " + str(lists[0]) + ", num_filters: " + str(lists[1]) + ", batch_size: " + str(lists[2]) +
+          ", M1D loss: " + str(lists[3]) + ", M1D Acc: " + str(lists[4]) + ", 1D loss: " + str(lists[5]) + ", 1D Acc: " + str(lists[6]), ", Time Taken: " + str(lists[7]))
--- scr/training/rnnChange.py
+++ scr/training/rnnChange.py
@@ -2,6 +2,7 @@ from scr.training.pretreatment import pretreatment, sentiment_predict, model_los
 from tensorflow.keras.layers import Embedding, Dense, LSTM, Bidirectional
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+import time
 
 
 class RNN:
@@ -16,7 +17,7 @@ class RNN:
                              save_best_only=True)
 
         model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-        history = model.fit(X_train, y_train, epochs=4, callbacks=[es, mc], batch_size=batch_size, validation_split=0.2)
+        history = model.fit(X_train, y_train, epochs=5, callbacks=[es, mc], batch_size=batch_size, validation_split=0.2)
 
         loaded_model = load_model('../../data/model/rnn_BiLSTM_best_model.h5')
         loss, final_acc = loaded_model.evaluate(X_test, y_test)
@@ -39,7 +40,7 @@ class RNN:
                              save_best_only=True)
 
         model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-        history = model.fit(X_train, y_train, epochs=4, callbacks=[es, mc], batch_size=batch_size, validation_split=0.2)
+        history = model.fit(X_train, y_train, epochs=5, callbacks=[es, mc], batch_size=batch_size, validation_split=0.2)
 
         loaded_model = load_model('../../data/model/rnn_LSTM_best_model.h5')
         loss, final_acc = loaded_model.evaluate(X_test, y_test)
@@ -63,18 +64,23 @@ class RNN:
 
         X_train, X_test, y_train, y_test, vocab_size, max_len, stopwords, tokenizer = pretreatment()
 
+        start = time.time()
         loaded_model_lstm, l_loss, final_acc_l = RNN.LstmLearn(embedding_dim, hidden_units, vocab_size, X_train, X_test,
                                                         y_train, y_test, batch_size)
         loaded_model_bilstm, b_loss, final_acc_b = RNN.BiLstmLearn(embedding_dim, hidden_units, vocab_size, X_train, X_test,
                                                             y_train, y_test, batch_size)
+        end = time.time() - start
 
         result.append(l_loss)
         result.append(final_acc_l)
         result.append(b_loss)
         result.append(final_acc_b)
+        result.append(end)
 
         # sentiment_predict("미친 새끼 또 저 지랄이네 대단하다 대단해", loaded_model_lstm, stopwords, tokenizer, max_len)
         # sentiment_predict("안녕하세요 오랜만에 뵈요. 어떤일 하고 있나요?", loaded_model_lstm, stopwords, tokenizer, max_len)
+
+        return result
 
 
 rnn = []
@@ -91,4 +97,4 @@ for i in range(0, 9, 2):
 
 for lists in rnn:
     print("embedding_dim: " + str(lists[0]) + ", hidden_units: " + str(lists[1]) + ", batch_size: " + str(lists[2]) +
-          ", LSTM loss: " + str(lists[3]) + ", LSTM Acc: " + str(lists[4]) + ", BiLSTM loss: " + str(lists[5]) + ", BiLSTM Acc: " + str(lists[6]))
+          ", LSTM loss: " + str(lists[3]) + ", LSTM Acc: " + str(lists[4]) + ", BiLSTM loss: " + str(lists[5]) + ", BiLSTM Acc: " + str(lists[6]) + ", Time Taken: " + str(lists[7]))
